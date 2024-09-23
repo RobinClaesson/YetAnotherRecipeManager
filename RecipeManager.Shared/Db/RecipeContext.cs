ebf8ed25717c09f99195567db881ec61aa0a83b6
Robin Claesson <robcla@kth.se>
2024-09-23 22:03:22 +0200

    Changed RecipeContext to take DbContextOptions

--- RecipeManager.Shared/Db/RecipeContext.cs
+++ RecipeManager.Shared/Db/RecipeContext.cs
@@ -7,20 +7,8 @@ public class RecipeContext : DbContext
 {
     public DbSet<Recipe> Recipes { get; set; }
 
-    public string DbPath { get; private set; } = Defaults.DbPath;
-
-    public RecipeContext()
+    public RecipeContext(DbContextOptions<RecipeContext> options) : base(options)
     {
-
-    }
-
-    public RecipeContext(string dbPath)
-    {
-        DbPath = dbPath;
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        options.UseSqlite($"Data Source={DbPath}");
+        
     }
 }
