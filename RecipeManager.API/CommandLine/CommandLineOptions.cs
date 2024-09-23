bdb7878abb4e9778f8597c1d17c04b335022b5c6
Robin Claesson <robcla@kth.se>
2024-09-23 22:41:35 +0200

    Added command line options for API

---
+++ RecipeManager.API/CommandLine/CommandLineOptions.cs
@@ -0,0 +1,41 @@
+﻿using CommandLine;
+using RecipeManager.Shared;
+
+namespace RecipeManager.API.CommandLine;
+
+[Verb("run", isDefault: true, HelpText = "Run the RecipeManager API")]
+public class RunOptions
+{
+    [Option('p', "http-port", Required = false, HelpText = $"The port to listen for HTTP requests.")]
+    public int? HttpPort { get; set; } = null;
+    public bool UseHttp => HttpPort != null;
+
+    [Option('s', "https-port", Required = false, HelpText = "The port to listen for HTTPS requests. HTTPS turned of if not set.")]
+    public int? HttpsPort { get; set; } = null;
+    public bool UseHttps => HttpsPort != null;
+
+    [Option('l', "localhost", Required = false, HelpText = "Allow connection only from localhost.")]
+    public bool LocalHost { get; set; } = false;
+
+    [Option('w', "swagger", Required = false, HelpText = "Allows direct API access with Swagger.")]
+    public bool UseSwagger { get; set; }
+
+    [Option('d', "db-path", Required = false, HelpText = $"The path to the SQLite database. Default: '{Defaults.DbPath}'")]
+    public string DbPath { get; set; } = Defaults.DbPath;
+
+    public string[] GetHostUrls()
+    {
+        var host = LocalHost ? "localhost" : "[::]";
+
+        //HTTP is enabled, but HTTPS is not
+        if (UseHttp && !UseHttps)
+            return [$"http://{host}:{HttpPort}"];
+
+        //HTTPS is enabled, but HTTP is not
+        if (!UseHttp && UseHttps)
+            return [$"https://{host}:{HttpsPort}"];
+
+        //Both HTTP and HTTPS are enabled
+        return [$"http://{host}:{HttpPort}", $"https://{host}:{HttpsPort}"];
+    }
+}
--- RecipeManager.API/Program.cs
+++ RecipeManager.API/Program.cs
@@ -1,35 +1,64 @@
+using CommandLine;
 using Microsoft.EntityFrameworkCore;
 using RecipeManager.API;
+using RecipeManager.API.CommandLine;
 using RecipeManager.API.Services;
 using RecipeManager.Shared.Db;
 
-var builder = WebApplication.CreateBuilder(args);
+Parser.Default.ParseArguments<RunOptions>(args)
+    .WithParsed(BuildAndRun)
+    .WithNotParsed(HandleParseError);
 
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<RecipeContext>(opt =>
+void BuildAndRun(RunOptions options)
 {
-    opt.UseSqlite($"Data Source={Settings.DbPath}");
-});
-builder.Services.AddScoped<IRecipesService, RecipesService>();
+    if(!options.UseHttp && !options.UseHttps)
+    {
+        Console.WriteLine("Error: Either HTTP or HTTPS port must be set");
+        return;
+    }
+
+    Settings.DbPath = options.DbPath;
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<RecipeContext>(opt =>
+    {
+        opt.UseSqlite($"Data Source={Settings.DbPath}");
+    });
+    builder.Services.AddScoped<IRecipesService, RecipesService>();
 
 
-var app = builder.Build();
+    // Add the ports from the command line options
+    builder.WebHost.UseUrls(options.GetHostUrls());
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (options.UseSwagger)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        Console.WriteLine("Swagger UI enabled.");
+    }
+
+    if (options.UseHttps)
+        app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+void HandleParseError(IEnumerable<Error> errors)
+{
+    Console.WriteLine("Failed to parse command line arguments:");
+    foreach (var error in errors)
+    {
+        Console.WriteLine(error);
+    }
+}
