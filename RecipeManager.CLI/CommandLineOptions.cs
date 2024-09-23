b6da1501caeebfca6cf1c5fd31ac40048ea2bd80
Robin Claesson <robcla@kth.se>
2024-09-23 23:07:31 +0200

    Added RecipeManager.CLI

---
+++ RecipeManager.CLI/CommandLineOptions.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+
+namespace RecipeManager.CLI;
+
+public abstract class BaseOptions
+{
+    [Option('h', "host", Required = true, HelpText = "Reipe Manager API Host")]
+    public string? Host { get; set; }
+
+    [Option('p', "port", Required = true, HelpText = "Reipe Manager API Host Port")]
+    public int Port { get; set; }
+
+    [Option('s', "use Https", HelpText = "Use HTTPS")]
+    public bool UseHttps { get; set; }
+}
+
+[Verb("list-recipes", HelpText = "List recipes")]
+public class ListRecipesOptions : BaseOptions
+{
+    [Option('t', "tag", HelpText = "Filter by tag")]
+    public string? Tag { get; set; }
+}
---
+++ RecipeManager.CLI/Program.cs
@@ -0,0 +1,51 @@
+﻿using CommandLine;
+using RecipeManager.CLI;
+using System.Net.Http.Json;
+
+await Parser.Default.ParseArguments<ListRecipesOptions>(args)
+    .MapResult(
+        (ListRecipesOptions options) => ListRecipes(options),
+        HandleParseError
+    );
+    
+
+async Task HandleParseError(IEnumerable<Error> errors)
+{
+    foreach (var error in errors)
+    {
+        Console.WriteLine(error);
+    }
+}
+
+async Task ListRecipes(ListRecipesOptions options)
+{
+    Console.WriteLine($"Fetching recipes from {options.Host}:{options.Port}");
+
+    using var client = new HttpClient();
+    client.BaseAddress = new UriBuilder
+    {
+        Scheme = options.UseHttps ? "https" : "http",
+        Host = options.Host,
+        Port = options.Port
+    }.Uri;
+
+    var result = await client.GetFromJsonAsync<List<string>>("/api/Recipe/RecipeNames");
+
+    if(result == null)
+    {
+        Console.WriteLine("Could not fetch recipes...");
+        return;
+    }
+
+    if(result.Count == 0)
+    {
+        Console.WriteLine("No recipes found");
+        return;
+    }
+
+    Console.WriteLine("Recipes:");
+    foreach (var recipe in result)
+    {
+        Console.WriteLine(recipe);
+    }
+}
