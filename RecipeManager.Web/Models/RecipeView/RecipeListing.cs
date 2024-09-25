7d4e5338c7c4d337d535850331ade27962a91e73
Robin Claesson <robcla@kth.se>
2024-09-25 21:22:22 +0200

    WEB Basic layout for recipe list

---
+++ RecipeManager.Web/Models/RecipeView/RecipeListing.cs
@@ -0,0 +1,20 @@
+﻿using RecipeManager.Shared.Models;
+
+namespace RecipeManager.Web.Models.RecipeView;
+
+public record RecipeListing : Recipe
+{
+    public string SourceName { get; set;} = string.Empty;
+
+    public RecipeListing(Recipe recipe, string sourceName)
+    {
+        RecipeId = recipe.RecipeId;
+        Name = recipe.Name;
+        Description = recipe.Description;
+        Tags = recipe.Tags;
+        Servings = recipe.Servings;
+        Ingredients = recipe.Ingredients;
+        Instructions = recipe.Instructions;
+        SourceName = sourceName;
+    }
+}
---
+++ RecipeManager.Web/Pages/RecipesPage.razor.cs
@@ -0,0 +1,28 @@
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using RecipeManager.Shared.Models;
+using RecipeManager.Web.Models.RecipeView;
+using RecipeManager.Web.Store.RecipeStore;
+using static MudBlazor.CategoryTypes;
+
+namespace RecipeManager.Web.Pages;
+
+public partial class RecipesPage
+{
+    [Inject]
+    IState<RecipeState> RecipeState { get; set; } = default!;
+
+    private IEnumerable<RecipeListing> RecipeListings 
+        => RecipeState.Value.RecipieCollections.
+                SelectMany(c =>
+                    c.Recipes.Select(r => new RecipeListing(r, c.Source?.Name ?? string.Empty))
+                );
+
+    private static string ReadableTags(RecipeListing recipe)
+        => string.Join(", ", recipe.Tags.OrderBy(t => t));
+
+    private void RecipeRowClicked(DataGridRowClickEventArgs<RecipeListing> args)
+    {
+    }
+}
