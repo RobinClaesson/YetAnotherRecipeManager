7ed58981595c40d1f5bd7fe12890f23db83be207
Robin Claesson <robcla@kth.se>
2024-09-29 12:38:41 +0200

    API Tests, added DatabaseVerifier

---
+++ RecipeManager.API.Tests/Verifiers/DatabaseVerifier.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using RecipeManager.Shared.Db;
+
+namespace RecipeManager.API.Tests.Verifiers;
+
+internal static class DatabaseVerifier
+{
+    public static void VerifyUnchangedDatabase(RecipeContext context)
+    {
+        context.Recipes.Count().Should().Be(MockDatabase.MockRecipes.Count);
+        context.Ingredients.Count().Should().Be(MockDatabase.MockIngredients.Count);
+        context.Instructions.Count().Should().Be(MockDatabase.MockInstructions.Count);
+
+        var resultDatabase = context.Recipes.Include(r => r.Ingredients)
+                                                    .Include(r => r.Instructions);
+
+        RecipeVerifier.VerifyRecipeIEnumerable(resultDatabase, MockDatabase.MockRecipes);
+    }
+}
