f6a66c5177150971e9bbe2c5b9e356d05ce0ef89
Robin Claesson <robcla@kth.se>
2024-09-29 14:47:17 +0200

    Added NavigationManager extensions

---
+++ RecipeManager.Web/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Specialized;
+
+namespace RecipeManager.Web.Extensions.Navbar;
+
+public static class NavigationManagerExtensions
+{
+    public static string CurrentPage(this NavigationManager navigationManager)
+        => navigationManager.ExtensionUri().Split('/').First();
+
+    public static string ExtensionUri(this NavigationManager navigationManager)
+        => navigationManager.Uri.Replace(navigationManager.BaseUri, "");
+}
